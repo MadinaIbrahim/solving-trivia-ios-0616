c694552e372ddfe5699d92231926d357ff913400
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2016-06-10 03:03:20 -0400

    Done

--- solving-trivia/FISAppDelegate.m
+++ solving-trivia/FISAppDelegate.m
@@ -8,7 +8,7 @@
     // Override point for customization after application launch.
     
     
-    [self solveTrivia];  //
+    [self solveTrivia];  // we are calling method to run it. self because this method is defind in this file. Two parts of this method call. Method name: solveTrivia, and self(receiver) where the definition of this method does exist. Whithin this file.
     
     return YES;
 }
@@ -24,7 +24,7 @@ Write your method here
 -(NSString *)solveTrivia {
     
     
-    
+    // we choose NSDictionary because relationship matters: Keys : Value pare always stay together, but the ordering of Key:Value pairs will be random.
     NSDictionary *StatesAndCapitaLs= @{@"Alabama":@"Montgomery",
                                    @"Alaska":@"Juneau",
                                    @"Arizona":@"Phoenix",
@@ -74,23 +74,38 @@ Write your method here
                                    @"Washington":@"Olympia",
                                    @"West Virginia":@"Charleston",
                                    @"Wisconsin":@"Madison",
-                                   @"Wyoming":@"Cheyenne"};
-    
-       // Search separate (or split) NSString by character:
-    
-//   The separator string can't be blank. If you need to separate a string into its individual characters, just loop through the length of the string and convert each char into a new string:
-    
-//    NSMutableArray *characters = [[NSMutableArray alloc] initWithCapacity:[myString length]];
-//    for (int i=0; i < [myString length]; i++) {
-//        NSString *ichar  = [NSString stringWithFormat:@"%c", [myString characterAtIndex:i]];
-//        [characters addObject:ichar];
-//    }
+                                   @"Wyoming":@"Cheyenne"}; // dose not matter if the list alphabetised or not.
     
     
     
     for (NSString *state in StatesAndCapitaLs) {
-        NSString *city = StatesAndCapitaLs[state]; //more generic than NSString *city = stateCapitals(@"Alabama");
+        NSString *city = StatesAndCapitaLs[state]; // To do the same for every single state : city pair. Iterating over StatesAndCapitaLs  Dictionary. NSString type *state because when you do a for in loop over dictionary you get the keys. Our keys are the state.
         
+        // NSString *city = StatesAndCapitaLs[@"Alabama"];
+        
+        // NSString *state = @"Alabama";
+    
+    
+        
+        
+// Search separate (or split) NSString by character:
+    
+//   The separator string can't be blank. If you need to separate a string into its individual characters, just loop through the length of the string and convert each char into a new string:
+    
+        
+        //    NSMutableArray *characters = [[NSMutableArray alloc] initWithCapacity:[myString length]]; // make new mutable array called characters
+    
+//it iterates all over the array
+    
+        //    for (NSInteger i=0; i < [myString length]; i++) {
+        //    NSString *ichar  = [NSString stringWithFormat:@"%c", [myString characterAtIndex:i]];
+        
+// NSString yields each character at index
+        
+        //    [characters addObject:ichar]; //and addes ichar character to characters array.
+        
+        //    }
+    
         
         //Get characters for the State
         
@@ -109,12 +124,15 @@ Write your method here
             [charactersInState addObject:ichar];
         }
         
-        BOOL correctAnswer = true;
+        BOOL correctAnswer = true; // We asume that this is correct answer.
         
         
         
-        
+        //NSLog(@"The Characters of the state are");
         for (NSString *stateCharacter in charactersInState) {
+            //NSLog(@"%@", stateCharacter);
+            
+            // the need to be inside of this loop because we want to iterate over the city for every single letter in our state, and only place I have  every letter individualized is inside of this loop
             
             // Get Characters for the City
             NSMutableArray *characterInCity = [[NSMutableArray alloc] initWithCapacity:[city length]];
@@ -127,22 +145,27 @@ Write your method here
             
             //Done getting characters
             
+            // NSLog(@"%@", characterInCity);
+            
             for (NSString *cityCharacter in characterInCity) {
                 
                 NSString *uppercaseCity = [cityCharacter uppercaseString];
                 NSString *uppercaseState = [stateCharacter uppercaseString];
                 
-                if ([uppercaseCity isEqualToString:uppercaseState]) {
-                    correctAnswer = false;
-                    NSLog(@"Not the correct answer because: %@ is in both", stateCharacter);
+                if ([uppercaseCity isEqualToString:uppercaseState]) { // Case - insensitive search to check if cityCharacter string is equal to stateCharacter string. If they are equal
+                    
+                    correctAnswer = false; // if it is not a corect answer we don't want NSLog. We want to continue to the next state : capital pair.
+                    
+                    // NSLog(@"Not the correct answer because: %@ is in both", stateCharacter);
                 }
             }
             
-            
+            // NSLog(@"%@", characterInCity);
             
         }
         if(correctAnswer){
             NSLog(@"%@ is the correct answer",state);
+            
             return state;
         }
         
